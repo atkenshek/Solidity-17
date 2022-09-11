38f776bbc6671cbaae8cd322a89a5a9e6d045198
Remix Workspace <>
2022-09-11 19:33:15 +0600

    Initial commit.

---
+++ contracts/bep-20.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Meiram Sopy Temirzhanov 2022-09-06 
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract MeiramCoin is ERC20{
+    constructor(uint256 initialSupply) ERC20("MeiramCoin", "MMM"){
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

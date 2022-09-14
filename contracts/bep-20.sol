cc008c1b8f352a6f3401880667c9230aa4464ebf
Remix Workspace <>
2022-09-14 23:17:23 +0600

    Updated little bit

--- contracts/bep-20.sol
+++ contracts/bep-20.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract MeiramCoin is ERC20{
-    constructor(uint256 initialSupply) ERC20("MeiramCoin", "MMM"){
+    constructor(uint256 initialSupply) ERC20("TassayCoin", "TMT"){
         _mint(msg.sender, initialSupply);
     }
 }
